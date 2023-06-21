ec51a23bd7da1c7eb311b4fb061488a908d79976
ice41 <edu.ice7@gmail.com>
2023-06-21 20:36:41 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'ice41'
+contact 'E-mail: geral@ice41.pt - Discord: discord.gg/eQgRv2BfxT'
+version '1.0.4'
+
+ui_page 'nui/darkside.html'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'hansolo.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'skywalker.lua'
+}
+
+files {
+	'nui/darkside.html',
+	'nui/lightsaber.js',
+	'nui/theforce.css',
+	'nui/teste.html'
+}
